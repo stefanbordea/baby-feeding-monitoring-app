621dd585cc8dceba64785c81d3c58238f5b4578f
Stefan Bordea <stefan@stefanbordea.com>
2023-06-06 14:34:41 +0300

    User Repository implementation

--- src/main/java/gr/athtech/babyfeedingmonitoringapp/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/gr/athtech/babyfeedingmonitoringapp/repository/implementation/UserRepositoryImpl.java
@@ -2,33 +2,72 @@ package gr.athtech.babyfeedingmonitoringapp.repository.implementation;
 
 import gr.athtech.babyfeedingmonitoringapp.model.User;
 import gr.athtech.babyfeedingmonitoringapp.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    private final Logger logger;
+
     @Override
-    public Optional<User> create(User type) {
-        return Optional.empty();
+    public Optional<User> create(User user) {
+        try {
+            entityManager.persist(user);
+            return Optional.of(user);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), "Error while creating user");
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<User> findById(Long id) {
-        return Optional.empty();
+        try {
+            User user = entityManager.find(User.class, id);
+            return Optional.ofNullable(user);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), "Error while retrieving user with ID: " + id);
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        try {
+            return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), "Error while retrieving all users");
+            return Collections.emptyList();
+        }
     }
 
     @Override
-    public Optional<User> update(User type) {
-        return Optional.empty();
+    public Optional<User> update(User user) {
+        try {
+            User userToBeUpdated = entityManager.merge(user);
+            return Optional.of(userToBeUpdated);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), "Error updating user with ID: " + user.getId());
+            return Optional.empty();
+        }
     }
 
     @Override
-    public void delete(User type) {
-
+    public void delete(User user) {
+        try {
+            entityManager.remove(entityManager.merge(user));
+        } catch (Exception e) {
+            logger.error(e.getMessage(), "Error removing user with ID: " + user.getId());
+        }
     }
 }
