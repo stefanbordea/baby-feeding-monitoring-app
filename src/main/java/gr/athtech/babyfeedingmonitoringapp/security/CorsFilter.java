1f59dd21d7f3d2e20f026c5a404a3be1e6f975a6
Stefan Bordea <stefan@stefanbordea.com>
2023-06-11 00:50:49 +0300

    Add cors filter

---
+++ src/main/java/gr/athtech/babyfeedingmonitoringapp/security/CorsFilter.java
@@ -0,0 +1,25 @@
+package gr.athtech.babyfeedingmonitoringapp.security;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8080");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "POST, GET, PUΤ, DELETE, PATCH, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "username, crossdomain, password, X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+
+
+    }
+}
