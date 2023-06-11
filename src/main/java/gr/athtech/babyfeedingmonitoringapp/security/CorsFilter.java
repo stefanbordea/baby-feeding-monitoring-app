ce6fc3b7223a30245d4a56eb95c522fa0aa3ce1a
Stefan Bordea <stefan@stefanbordea.com>
2023-06-11 21:19:57 +0300

    update cors filter

--- src/main/java/gr/athtech/babyfeedingmonitoringapp/security/CorsFilter.java
+++ src/main/java/gr/athtech/babyfeedingmonitoringapp/security/CorsFilter.java
@@ -13,13 +13,15 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "username, crossdomain, password, X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
-        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
-
-
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
